cff18ae1ad325b149d56df8376fe168dff79d7a9
naro19 <naro91@mail.ru>
2015-01-18 20:15:43 +0300

    обработка некоторых ошибок

--- src/main/java/frontend/Frontend.java
+++ src/main/java/frontend/Frontend.java
@@ -7,6 +7,7 @@ import clasesForExecutionQuery.User;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import clasesForExecutionQuery.General;
+import com.google.gson.JsonSyntaxException;
 import database.Database;
 
 import javax.servlet.ServletException;
@@ -41,13 +42,18 @@ public class Frontend extends HttpServlet {
 
         response.setContentType("text/html;charset=utf-8");
         json = getRequestParser(URLDecoder.decode(request.getQueryString(), "UTF-8")); // получение json из get запроса
-        try {
-            responseResult = delegationCall(request.getRequestURI().split("/"), json);  // делегирование запроса соответствующему классу и методу
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (json.get("code") == null) {
+            try {
+                responseResult = delegationCall(request.getRequestURI().split("/"), json);  // делегирование запроса соответствующему классу и методу
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            responseSetstatus(responseResult, response);  // установление статуса ответа в зависимости от результата выполнения запроса
+            response.getOutputStream().print(responseResult); // отправка ответа
+        } else {
+            responseSetstatus(json.toString(), response);  // установление статуса ответа в зависимости от результата выполнения запроса
+            response.getOutputStream().print(json.toString()); // отправка ответа
         }
-        responseSetstatus(responseResult, response);  // установление статуса ответа в зависимости от результата выполнения запроса
-        response.getOutputStream().print(responseResult); // отправка ответа
         response.getOutputStream().flush();
         System.out.println(responseResult);
     }
@@ -57,13 +63,18 @@ public class Frontend extends HttpServlet {
 
         response.setContentType("text/html;charset=utf-8");
         json = requestGetJson(request);  // получение json из запроса
-        try {
-            responseResult = delegationCall(request.getRequestURI().split("/"), json);  // делегирование запроса соответствующему классу и методу
-        } catch (SQLException e) {
-            System.out.println(e);
+        if (json.get("code") == null) {
+            try {
+                responseResult = delegationCall(request.getRequestURI().split("/"), json);  // делегирование запроса соответствующему классу и методу
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+            responseSetstatus(responseResult, response);  // установление статуса ответа в зависимости от результата выполнения запроса
+            response.getOutputStream().print(responseResult); // отправка ответа
+        } else {
+            responseSetstatus(json.toString(), response);  // установление статуса ответа в зависимости от результата выполнения запроса
+            response.getOutputStream().print(json.toString()); // отправка ответа
         }
-        responseSetstatus(responseResult, response);  // установление статуса ответа в зависимости от результата выполнения запроса
-        response.getOutputStream().print(responseResult); // отправка ответа
         response.getOutputStream().flush();
         System.out.println(responseResult);
     }
@@ -87,28 +98,33 @@ public class Frontend extends HttpServlet {
                         return user.delegationCall(requestMass[4], json);
                     case "thread":
                         return thread.delegationCall(requestMass[4], json);
-                    default: return "bad";
+                    default: return "{code : 3, response : \"invalid query\"}";
                 }
-            } else return "bad";
+            } else return "{code : 3, response : \"invalid query\"}";
         }
 
     }
 
     private JsonObject requestGetJson (HttpServletRequest request) throws IOException { // функция для считывания json в запросе
         StringBuilder json = new StringBuilder();
+        JsonObject jsonObject;
         String temp;
         while ((temp = request.getReader().readLine()) != null) {
             json.append(temp);
         }
-        return gson.fromJson(json.toString(), JsonObject.class);
+        try {
+            jsonObject = gson.fromJson(json.toString(), JsonObject.class);
+        } catch (JsonSyntaxException e) {
+            jsonObject = new JsonObject();
+            jsonObject.addProperty("code", 2);
+            jsonObject.addProperty("response", "invalid request");
+        }
+        return jsonObject;
     }
 
     private void responseSetstatus (String status, HttpServletResponse response) { // функция для установления статуса ответа
         switch (status) {
-            case "bad1":
-                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                break;
-            case "bad":
+            case "fail":
                 response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 break;
             default: response.setStatus(HttpServletResponse.SC_OK);
@@ -122,18 +138,25 @@ public class Frontend extends HttpServlet {
         String strings[] = data.split("&");
         ArrayList<String> array = new ArrayList<>();
 
-        for(int i = 0; i < strings.length; i++) {
+        for (int i = 0; i < strings.length; i++) {
             String values[] = strings[i].split("=");
-
-            if (jsonObject.get(values[0]) != null) {
-                if (array.size() == 0) array.add(jsonObject.get(values[0]).getAsString());
-                array.add(values[1]);
-                jsonObject.addProperty(values[0], array.toString());
-            } else {
-                array.clear();
-                jsonObject.addProperty(values[0], values[1]);
+            if (values.length == 2) {
+                if (jsonObject.get(values[0]) != null) {
+                    if (array.size() == 0) array.add(jsonObject.get(values[0]).getAsString());
+                    array.add(values[1]);
+                    jsonObject.addProperty(values[0], array.toString());
+                } else {
+                    array.clear();
+                    jsonObject.addProperty(values[0], values[1]);
+                }
+            }else {
+                jsonObject = new JsonObject();
+                jsonObject.addProperty("code", 2);
+                jsonObject.addProperty("response", "invalid request");
+                return jsonObject;
             }
         }
+
         return jsonObject;
     }
 }
