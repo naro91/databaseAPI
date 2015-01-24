9e26981ff3e05dcac2696f7b024f0cb7ed491c10
naro19 <naro91@mail.ru>
2015-01-24 16:14:31 +0300

    c

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 
+import com.sun.xml.internal.ws.api.policy.PolicyResolver;
 import frontend.Frontend;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 /**
  * Created by Abovyan Narek on 06.11.14.
@@ -11,7 +14,13 @@ public class Main {
 
     public static void main(String args[]) throws Exception{
         Frontend frontend = new Frontend();
-        Server server = new Server(8080);
+        QueuedThreadPool threadPool = new QueuedThreadPool();
+        threadPool.setMinThreads(10);
+        threadPool.setMinThreads(100);
+        Server server = new Server(threadPool);
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
         context.addServlet(new ServletHolder(frontend), "/*");
